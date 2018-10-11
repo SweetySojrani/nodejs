dd6006397d02445bdb1a7aba1c340c9038ffe116
Sweety Sojrani <sweety.sojrani@sjsu.edu>
2018-10-11 13:26:15 -0700

    Update app.js

--- app.js
+++ app.js
@@ -54,11 +54,11 @@ var db_pwd  = "cmpe281" ;
 var db_name = "test" ;
 */
 
-var db_host = (process.env.mongodb_host || "localhost" ) ;
-var db_port = (process.env.mongodb_port || "27017" ) ;
+var db_host = (process.env.mongodb_host || "mongodb://cmpe281:cmpe281@ds127843.mlab.com:27843/heroku_hn3nlzvz" ) ;
+var db_port = (process.env.mongodb_port || "27843" ) ;
 var db_user = (process.env.mongodb_user || "cmpe281" ) ;
 var db_pwd  = (process.env.mongodb_pwd  || "cmpe281" ) ;
-var db_name = (process.env.mongodb_name || "test" ) ;
+var db_name = (process.env.mongodb_name || "heroku_hn3nlzvz" ) ;
    
 
 var db = new DB(db_name,
